99e4c370189504657e30dde5656acf75756a6548
Deniska <dumbrovskijdeniska@gmail.com>
2025-07-13 13:06:21 +0500

    Улучшение: добавлен комментарий 4

--- calculyator.py
+++ calculyator.py
@@ -33,7 +33,7 @@ while True:
             num1 = float(input("Введите первое число: "))
             num2 = float(input("Введите второе число: "))
         except ValueError:
-            print("444. Пожалуйста, введите числа.")
+            print("555. Пожалуйста, введите числа.")
             continue
 
         if choice == '1':
